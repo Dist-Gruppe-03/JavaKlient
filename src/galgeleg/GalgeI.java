73c7bd284cbeccd42f87881244eb6f4670c86fe9
KredeDK <krede92@gmail.com>
2018-05-12 06:35:37 +0200

    GalgeI update

--- src/galgeleg/GalgeI.java
+++ src/galgeleg/GalgeI.java
@@ -1,15 +1,19 @@
 package galgeleg;
-import java.rmi.RemoteException;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Map;
-
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
 public interface GalgeI extends java.rmi.Remote {
-
+    
+    @WebMethod public int getPersonalHighscore(String username)throws java.rmi.RemoteException, ClassNotFoundException, SQLException;
+    
+    @WebMethod public String[][] getHighscores() throws java.rmi.RemoteException, ClassNotFoundException, SQLException;
+    
+    @WebMethod public String getName(String username) throws java.rmi.RemoteException, ClassNotFoundException, SQLException;
+    
     @WebMethod public ArrayList<String> getBrugteBogstaver(String bruger) throws java.rmi.RemoteException;
 
     @WebMethod public String getSynligtOrd(String bruger) throws java.rmi.RemoteException;
@@ -37,10 +41,4 @@ public interface GalgeI extends java.rmi.Remote {
     @WebMethod boolean hentBruger(String brugernavn, String adgangskode) throws java.rmi.RemoteException;
 
     @WebMethod public void highscoreCheck(String bruger, int score) throws java.rmi.RemoteException, ClassNotFoundException, SQLException;
-    
-    @WebMethod public void hentOrdFraDr(String bruger) throws java.rmi.RemoteException;
-    
-    @WebMethod public String getName(String username) throws java.rmi.RemoteException, ClassNotFoundException, SQLException;
-    
-    @WebMethod public int getPersonalHighscore(String username) throws RemoteException, ClassNotFoundException, SQLException;
 }
